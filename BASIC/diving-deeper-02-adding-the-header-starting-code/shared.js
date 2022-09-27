aff029bc48ea79aa26a8d0d80148ab718400392d
Vipul K Mishra <Vipul.Mishra@gdit.com>
2022-09-27 14:41:00 -0400

    09/27

--- BASIC/diving-deeper-02-adding-the-header-starting-code/shared.js
+++ BASIC/diving-deeper-02-adding-the-header-starting-code/shared.js
@@ -14,34 +14,50 @@ for(var i=0;i<plansButtons.length;i++)
 
     plansButtons[i].addEventListener('click',function(){
 
-         modal.style.display='block';
-         backdrop.style.display='block';
+        //  modal.style.display='block';
+        //  backdrop.style.display='block';
+        // modal.className='open'; This will overrite the complete class name.
+        modal.classList.add('open');
+        backdrop.classList.add('open');
+
     });
 }
 
 var clickNoButton=document.querySelector('.modal__action--negative');
 console.dir(clickNoButton);
 
+if(clickNoButton){
+    clickNoButton.addEventListener('click',closeModal);
+}
 
-clickNoButton.addEventListener('click',closeModal);
 
 backdrop.addEventListener('click',function(){
-    mobileNav.style.display='none';
+    // mobileNav.style.display='none';
+    mobileNav.classList.remove('open');
     closeModal();
 
 });
 
 function closeModal(){
 
-    modal.style.display='none';
-    backdrop.style.display='none';
+    // modal.style.display='none';
+    // backdrop.style.display='none';
+
+    if(modal){
+        modal.classList.remove('open');
+    }
+    
+    backdrop.classList.remove('open');
 }
 
 
 toggleButton.addEventListener('click',function(){
 
 
-    mobileNav.style.display='block';
-    backdrop.style.display='block';
+    // mobileNav.style.display='block';
+    // backdrop.style.display='block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+
 
 });
